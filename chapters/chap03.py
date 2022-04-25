eeaf5695b6f7bfa3d90ec616a38ebe754db4399e
Nguyen Quang Chien <nguyenquangchien@gmail.com>
2022-04-25 22:25:50 +0100

    Update chap03.py
    Vietnamese translation

--- chapters/chap03.py
+++ chapters/chap03.py
@@ -1,11 +1,11 @@
 from modsim import *
 
 def step(state, p1, p2):
-    """Simulate one time step.
+    """Mô phỏng 1 bước thời gian.
     
-    state: bikeshare State object
-    p1: probability of an Olin->Wellesley ride
-    p2: probability of a Wellesley->Olin ride
+    state: đối tượng State chỉ trạng thái dùng chung xe
+    p1: xác suất có xe theo hướng Olin->Wellesley
+    p2: xác suất có xe theo hướng Wellesley->Olin
     """
     if flip(p1):
         bike_to_wellesley(state)
@@ -16,9 +16,9 @@ def step(state, p1, p2):
 from modsim import *
 
 def bike_to_olin(state):
-    """Move one bike from Wellesley to Olin.
+    """Di chuyển một xe từ Wellesley đến Olin.
     
-    state: bikeshare State object
+    state: đối tượng State chỉ trạng thái dùng chung xe
     """
     if state.wellesley == 0:
         state.wellesley_empty += 1
@@ -28,12 +28,12 @@ def bike_to_olin(state):
 
 from modsim import *
 
-# Solution
+# Lời giải
 
 def bike_to_wellesley(state):
-    """Move one bike from Olin to Wellesley.
+    """Di chuyển một xe từ Olin đến Wellesley.
     
-    state: bikeshare State object
+    state: đối tượng State chỉ trạng thái dùng chung xe
     """
     if state.olin == 0:
         state.olin_empty += 1
